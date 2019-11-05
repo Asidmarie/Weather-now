736ea65beb01fc7953f33b46abe90fcbabe885a4
Asidmarie <Asidmarie.aps@gmail.com>
2019-11-05 10:47:29 -0500

    BodyParser

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Weather-now
+
+get user input of the city
+get the weather for city 
+    if error let the user know there was an error 
+    if theres is no weather availble error 
+    else store the weather for the user city
+show weather or error
+
--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
- 
+ const bodyParser = require('body-parser')
 
 //setting view engine
 app.set('view engine', 'ejs');
 // middleware
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false}));
 /*
     ROUTES
 */
@@ -14,7 +15,10 @@ app.use(express.static('./public'));
 
 app.get('/', function (req, res) {
   res.render('home.ejs');
-})
+});
+app.post("/whatever-we-want", function(req,res){
+  console.log(req)
+});
  
 app.listen(3000, function(){
     console.log('server is live on port: 3000');
